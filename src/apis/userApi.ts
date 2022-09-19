7f3ead2f2fc87f34e4f6fb6f8770db62d4374d31
Enziandom <zhengrenfu@outlook.com>
2022-09-19 21:02:37 +0800

    :ambulance: 删除多余的代码

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,34 +1,38 @@
-import { normalizePost, NormalizeAxiosError, NormalizeAxiosSuccess } from "@/service/userService";
+import { normalizePost, NormalizeCallback } from "@/service/userService";
 import { UserModel } from "@/models";
 
 export function checkUser(
   data: UserModel,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
+  success?: NormalizeCallback,
+  error?: NormalizeCallback
 ): any {
-  return normalizePost({ url: "/login", data, config: { withCredentials: true } }, success, error);
+  return normalizePost(
+    { method: "post", url: "/login", data, withCredentials: true },
+    success,
+    error
+  );
 }
 
 export function updateUser(
   data: UserModel,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
+  success?: NormalizeCallback,
+  error?: NormalizeCallback
 ): any {
-  return normalizePost({ url: "/update/user", data }, success, error);
+  return normalizePost({ method: "post", url: "/update/user", data }, success, error);
 }
 
 export function queryFriends(
   data: UserModel,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
+  success?: NormalizeCallback,
+  error?: NormalizeCallback
 ): any {
-  return normalizePost({ url: "/query/friends", data }, success, error);
+  return normalizePost({ method: "post", url: "/query/friends", data }, success, error);
 }
 
 export function queryGroups(
   data: UserModel,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
+  success?: NormalizeCallback,
+  error?: NormalizeCallback
 ): any {
-  return normalizePost({ url: "/query/groups", data }, success, error);
+  return normalizePost({ method: "post", url: "/query/groups", data }, success, error);
 }
--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,57 +1,17 @@
 import { AxiosRequestConfig } from "axios";
 import { request } from "@/apis";
-import { UserModel } from "@/models";
 
-export interface NormalizeAxiosSuccess {
-  (response: NormalizeResponse): void;
-}
-
-interface NormalizeResponse {
-  status?: number;
-  data: any;
-}
-
-export interface NormalizeAxiosError {
-  (error: any): void;
-}
-
-export interface NormalizeAxiosOptions {
-  url: string;
-  data?: UserModel;
-  config?: AxiosRequestConfig;
-}
-
-export function normalizeGet(
-  ops: NormalizeAxiosOptions,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
-): any {
-  if (success || error) {
-    return request
-      .get(ops.url, ops.config)
-      .then(({ data: res }) => {
-        if (res.status == 200) {
-          success && success(res);
-        } else {
-          error && error(res);
-        }
-      })
-      .catch(err => {
-        error && error(err);
-      });
-  } else {
-    return request.get(ops.url, ops.config);
-  }
+export interface NormalizeCallback {
+  (data: any, code?: number): void;
 }
 
 export function normalizePost(
-  ops: NormalizeAxiosOptions,
-  success?: NormalizeAxiosSuccess,
-  error?: NormalizeAxiosError
+  ops: AxiosRequestConfig,
+  success?: NormalizeCallback,
+  error?: NormalizeCallback
 ): any {
   if (success || error) {
-    return request
-      .post(ops.url, ops.data, ops.config)
+    return request(ops)
       .then(({ data: res }) => {
         if (res.status == 200) {
           success && success(res);
@@ -63,6 +23,6 @@ export function normalizePost(
         error && error(err);
       });
   } else {
-    return request.post(ops.url, ops.data, ops.config);
+    return request(ops);
   }
 }
